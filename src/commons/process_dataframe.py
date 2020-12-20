85641131317c59b9d2712fdea196a6792d483087
miaoli-psy <chuoli223@yahoo.com>
2020-12-20 23:21:28 +0100

    rm is_debug

--- src/commons/process_dataframe.py
+++ src/commons/process_dataframe.py
@@ -1,32 +1,33 @@
 import pandas as pd
 
+# Notice: func_name is process each element in each column, instead of the whole column
 
-def process_col(input_df: pd.DataFrame, col_name: str, func_name, is_debug=True):
+def process_col(input_df: pd.DataFrame, col_name: str, func_name):
     if col_name in input_df.columns:
         input_df[col_name] = input_df[col_name].map(func_name)
-    elif is_debug:
+    else:
         raise Exception(f"Warning: missing {col_name}")
 
 
-def process_cols(input_df: pd.DataFrame, input_cols: list, func_name, is_debug=True):
+def process_cols(input_df: pd.DataFrame, input_cols: list, func_name):
     for col in input_cols:
         if col in input_df.columns:
             input_df[col] = input_df[col].map(func_name)
-        elif is_debug:
+        else:
             raise Exception(f"Warning: Missing {col}")
 
 
-def insert_new_col(input_df: pd.DataFrame, old_col: str, new_col: str, func_name, is_debug=True):
+def insert_new_col(input_df: pd.DataFrame, old_col: str, new_col: str, func_name):
     if old_col in input_df.columns:
         col_index = input_df.columns.get_loc(old_col)
         input_df.insert(col_index, new_col, input_df[old_col].map(func_name))
-    elif is_debug:
+    else:
         raise Exception(f"Warning: missing {old_col}")
 
 
-def insert_new_col_from_two_cols(input_df: pd.DataFrame, old_col1: str, old_col2, new_col: str, func_name, is_debug=True):
+def insert_new_col_from_two_cols(input_df: pd.DataFrame, old_col1: str, old_col2, new_col: str, func_name):
     cols = input_df.columns
     if (old_col1 in cols) and (old_col2 in cols):
         input_df[new_col] = input_df.apply(lambda x: func_name(x[old_col1], x[old_col2]), axis = 1)
-    elif is_debug:
+    else:
         raise Exception(f"Warning: missing {old_col1} or {old_col2}")

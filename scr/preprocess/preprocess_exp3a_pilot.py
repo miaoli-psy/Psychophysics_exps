1f80429b23bf4f30170a2ae59f92776f32d9aec9
miaoli-psy <chuoli223@yahoo.com>
2020-12-20 20:52:04 +0100

    exp3a pilot preprocessed

--- scr/preprocess/preprocess_exp3a_pilot.py
+++ scr/preprocess/preprocess_exp3a_pilot.py
@@ -1,6 +1,7 @@
 from scr.preprocess.sub import merge_all_data
 import pandas as pd
 import copy
+import math
 
 
 def preprocess_exp3a_func(data_path: str, filetype: str, filename_prefix: str) -> pd.DataFrame:
@@ -29,8 +30,41 @@ def drop_df_nan_rows_according2cols(df: pd.DataFrame, cols: list) -> pd.DataFram
     return df
 
 
+def drop_df_rows_according2_one_col(df: pd.DataFrame, col_name: str, lowerbondary: float,
+                                    upperbondary: float) -> pd.DataFrame:
+    """
+    :param df:
+    :param col:  column name(str)
+    :param lowerbondary:
+    :param upperbondary:
+    :return:
+    """
+    df = df[(df[col_name] < upperbondary) & (df[col_name] > lowerbondary)]
+    return df
+
+
+def __cal_std_of_one_col(df: pd.DataFrame, col_name: str) -> float:
+    std = df[col_name].std()
+    return std
+
+
+def __cal_mean_of_one_col(df: pd.DataFrame, col_name: str) -> float:
+    mean = df[col_name].mean()
+    return mean
+
+
+# def get_resp_boundary(mean: float, std: float, n_std = 2) -> tuple:
+#     return mean - n_std * std, mean + n_std * std
+
+
+def get_col_boundary(df: pd.DataFrame, col_name: str, n_std = 2) -> tuple:
+    mean = __cal_mean_of_one_col(df, col_name)
+    std = __cal_std_of_one_col(df, col_name)
+    return mean - n_std * std, mean + n_std * std
+
 if __name__ == "__main__":
     is_debug = False
+    write_to_excel = False
     data_path = "../../data/rawdata_exp3a_pilot/"
     filename_prefix = "P"
     filetype = ".csv"
@@ -73,8 +107,23 @@ if __name__ == "__main__":
     all_df = keep_valid_columns(all_df, kept_col_names)
 
     # drop practice trials: drop all rows with NaNs in key_resp.keys
-    subset = ['key_resp.keys']
-    d_all_df = drop_df_nan_rows_according2cols(all_df, subset)
+    col_to_dropna = ['key_resp.keys']
+    all_df = drop_df_nan_rows_according2cols(all_df, col_to_dropna)
+
+    # drop too fast and too slow response
+    col_to_drop_rows = "key_resp.rt"
+    min_rt = 0.15
+    max_rt = 3
+    all_df = drop_df_rows_according2_one_col(all_df, col_to_drop_rows, min_rt, max_rt)
+
+    # drop response that are outside 2 standard deviation
+    col_rt = "key_resp.rt"
+    boundary = get_col_boundary(all_df, col_rt)
+    resp_min = boundary[0]
+    resp_max = boundary[1]
+    all_df = drop_df_rows_according2_one_col(all_df, col_rt, resp_min, resp_max)
 
     if is_debug:
         col_names = list(all_df.columns)
+    if write_to_excel:
+        all_df.to_excel("preprocess_exp3a_pilot.xlsx")

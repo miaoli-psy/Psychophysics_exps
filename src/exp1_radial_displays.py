e918ddb52c89914d239204de7db82cd83336951c
miaoli-psy <chuoli223@yahoo.com>
2020-12-31 00:28:11 +0100

    calculated alignmnet values and number of sectors to df

--- src/exp1_radial_displays.py
+++ src/exp1_radial_displays.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     if has_indi_alinement_value:
         # (3) get the current range_list for each display - to draw
         curr_step = 12  # degree step: 0-12 (in theory 0-360)
-        curr_countlist_index = 248 # 0-249 (250 displays)
+        curr_countlist_index = 248  # 0-249 (250 displays)
         # [[[angle, angle+step), num_points1], [[angle+1, angle+step+1), num_points1], etc]
         curr_rangelist = get_current_rangelist_to_draw(step_ranges_map, curr_step, curr_countlist_index)
 
@@ -43,23 +43,32 @@ if __name__ == '__main__':
         # n2: number of ray that contains n number of discs
         curr_alignment_value, n_sectors = get_alignment_value(curr_rangelist, angle = 6, weight = [0, 0, 2, 3, 4, 5])
 
+    # calculate alignment values (at given angle step:  6, 12 deg)
+    included_step_range = [6, 12]
+    sub_step_range_map = {k: v for k, v in step_ranges_map.items() if k in included_step_range}
+    weight = [0, 0, 2, 3, 4, 5]
 
-    # calculate alignment values (at given angle step: 0, 6, 12 deg)
-    # included_step_range = [0, 6, 12]
-    # sub_step_range_map = {k: v for k, v in step_ranges_map.items() if k in included_step_range}
-    #
-    # alignment_value_dict = dict()
-    # for step_range, all_range_list in sub_step_range_map.items():
-    #     alignment_values = list()
-    #     for range_list in all_range_list:
-    #         alignment_value = get_alignment_value_per_display(range_list)
-    #         alignment_values.append(alignment_value)
-    #     alignment_value_dict.update({step_range: alignment_values})
-    #
-    # # append alignment values to dataframe
-    # simuli_df["alignment_v_step_0"] = alignment_value_dict[0]
-    # simuli_df["alignment_v_step_6"] = alignment_value_dict[6]
-    # simuli_df["alignment_v_step_12"] = alignment_value_dict[12]
+    alignment_value_dict = dict()
+    # key: angle step
+    # value: list of 250 alignment value
+    n_sectors_dict = dict()
+    # key: angle step
+    # value: list of 250 lists of 6 sector numbers
+    for step_range, all_range_list in sub_step_range_map.items():
+        alignment_values = list()
+        n_sectors_list = list()
+        for range_list in all_range_list:
+            alignment_value, n_sectors = get_alignment_value(range_list, angle = step_range, weight = weight)
+            alignment_values.append(alignment_value)
+            n_sectors_list.append(n_sectors)
+        alignment_value_dict.update({step_range: alignment_values})
+        n_sectors_dict.update({step_range: n_sectors_list})
+
+    # append alignment values to dataframe
+    simuli_df["alignment_v_step_6"] = alignment_value_dict[6]
+    simuli_df["alignment_v_step_12"] = alignment_value_dict[12]
+    simuli_df["alignment_n_sectors_step_6"] = n_sectors_dict[6]
+    simuli_df["alignment_n_sectors_step_12"] = n_sectors_dict[12]
 
     if write_to_excel:
         simuli_df.to_excel("exp1_stim_info.xlsx")

7f34d1654afd9f139d3d1880ca79c448312f6516
miaoli-psy <chuoli223@yahoo.com>
2021-01-15 23:58:51 +0100

    new way to calcualte beams number

--- src/analysis/exp1_raidal_displays_analysis2.py
+++ src/analysis/exp1_raidal_displays_analysis2.py
@@ -6,6 +6,10 @@ Create time: 2021-01-15 17:26
 IDE: PyCharm
 Introduction:
 """
+from collections import Counter
+
+from src.commons.process_str import str_to_list
+from src.point.polar_point import get_polar_coordinates
 
 
 def get_angle_range(polar_posi_list, ini_start_angle = 0, ini_end_angle = 12):
@@ -49,8 +53,26 @@ def count_ndisc_in_range(polar_posi_list, range_start, range_end):
         for polar_posi in polar_posi_list:
             if range_start <= polar_posi[0] < 360:
                 count += 1
-                print(polar_posi)
             elif 0 <= polar_posi[0] < range_end:
                 count += 1
-                print(polar_posi)
     return count
+
+
+def counter2list(input_counter):
+    return [input_counter[1], input_counter[2], input_counter[3], input_counter[4], input_counter[5], input_counter[6]]
+
+
+def get_beam_n(input_posi_list, angle_size = 12):
+
+    input_posi_list = str_to_list(input_posi_list)
+    polar_posis = get_polar_coordinates(input_posi_list)
+    ini_start_angle = polar_posis[0][0]
+    ini_end_angle = ini_start_angle + angle_size
+    ranges = get_angle_range(polar_posis, ini_start_angle = ini_start_angle, ini_end_angle = ini_end_angle)
+    ndisc_list = list()
+    for beam in ranges:
+        ndisc = count_ndisc_in_range(polar_posis, beam[0], beam[1])
+        ndisc_list.append(ndisc)
+    count_beams = Counter(ndisc_list)
+    count_beams_output = counter2list(count_beams)
+    return count_beams_output
--- src/exp1_radial_display2.py
+++ src/exp1_radial_display2.py
@@ -7,9 +7,10 @@ IDE: PyCharm
 Introduction:
 """
 import pandas as pd
+from collections import Counter
 
-from src.analysis.exp1_raidal_displays_analysis2 import get_angle_range, count_ndisc_in_range
-from src.commons.process_dataframe import get_col_names
+from src.analysis.exp1_raidal_displays_analysis2 import get_angle_range, count_ndisc_in_range, get_beam_n, counter2list
+from src.commons.process_dataframe import get_col_names, insert_new_col
 from src.commons.process_str import str_to_list
 from src.point.polar_point import get_polar_coordinates
 
@@ -17,24 +18,22 @@ if __name__ == '__main__':
     is_debug = True
     indi_display = True
 
-    # Read stimuli display
+    # read stimuli display
     PATH = "../displays/"
     FILENAME = "update_stim_info_full.xlsx"
     stimuli_df = pd.read_excel(PATH + FILENAME)
+    # get and insert new col "n_beams" into stimuli dataframe
+    # number of beams that contains 1, 2, 3, 4, 5, 6 disc
+    insert_new_col(stimuli_df, "positions_list", "n_beams", get_beam_n)
 
     # individual display alignment value
     if indi_display:
-        display_n = 51 # 0-249
-        posis = stimuli_df.positions_list[display_n]
-        posis = str_to_list(posis)
+        display_n = 120 # 0-249
+        posis_str = stimuli_df.positions_list[display_n]
+        posis = str_to_list(posis_str)
         # get polar positions for a single display
         polar_posis = get_polar_coordinates(posis)
 
-        # check how close between the nearest two disc
-        diff = []
-        for index, polar_posi in enumerate(polar_posis):
-            if index > 0:
-                diff.append(polar_posi[0]-polar_posis[index-1][0])
         # some parameters, started and ended where
         ini_start_angle = polar_posis[0][0]
         angle_size = 12
@@ -46,9 +45,9 @@ if __name__ == '__main__':
         for beam in my_range:
             ndisc = count_ndisc_in_range(polar_posis, beam[0], beam[1])
             ndisc_list.append(ndisc)
-
-
-
+        # get number of beams that contains 1, 2, 3, 4, 5, 6 discs
+        count_beams = Counter(ndisc_list)
+        n_beams = counter2list(count_beams)
 
     if is_debug:
         col_names = get_col_names(stimuli_df)

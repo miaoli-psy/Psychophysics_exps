e6815a42f9f5f320c892c3f1d549563825664737
miaoli-psy <chuoli223@yahoo.com>
2021-03-04 13:21:30 +0100

    exp1 analysis anova with posthoc

--- src/stat_tests/exp1_rerun_anova.py
+++ src/stat_tests/exp1_rerun_anova.py
@@ -6,3 +6,23 @@ Create time: 2021-03-03 15:21
 IDE: PyCharm
 Introduction:
 """
+import pandas as pd
+import pingouin as pg
+
+if __name__ == '__main__':
+    PATH = "../../data/exp1_rerun_data/"
+    DATA = "cleanedTotalData_fullinfo_v3.xlsx"
+    mydata = pd.read_excel(PATH + DATA)
+
+    # 2 way annova
+    aov = pg.rm_anova(dv = "deviation_score",
+                      within = ["winsize", "crowdingcons"],
+                      subject = "participant_N",
+                      data = mydata)
+    # post hoc
+    posthocs = pg.pairwise_ttests(dv = "deviation_score",
+                                   within = ["winsize", "crowdingcons"],
+                                   subject = "participant_N",
+                                   data = mydata,
+                                   padjust = "holm",
+                                   effsize = "cohen")

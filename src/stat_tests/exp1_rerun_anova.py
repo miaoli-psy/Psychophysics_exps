db1c3c06bb0ea37e356f633fe253221d6196580b
miaoli-psy <chuoli223@yahoo.com>
2021-04-05 15:56:41 +0200

    ms1 stat text with new preprocessed data 3std

--- src/stat_tests/exp1_rerun_anova.py
+++ src/stat_tests/exp1_rerun_anova.py
@@ -9,10 +9,39 @@ Introduction: ms anova and pairwise comparison
 import pandas as pd
 import pingouin as pg
 
+from src.commons.process_dataframe import get_sub_df_according2col_value
+
+
+def cal_ds_mean(df, crowdingcon):
+    """
+    calculate
+    """
+    if crowdingcon == 1:
+        return get_sub_df_according2col_value(df, "crowdingcons", 1).mean()
+    elif crowdingcon == 0:
+        return get_sub_df_according2col_value(df, "crowdingcons", 0).mean()
+    else:
+        Exception(f"crowdingcon = {crowdingcon} is not recognized, use 1 or 0")
+
+
+def cal_ds_std(df, crowdingcon):
+    if crowdingcon == 1:
+        return get_sub_df_according2col_value(df, "crowdingcons", 1).std()
+    elif crowdingcon == 0:
+        return get_sub_df_according2col_value(df, "crowdingcons", 0).std()
+    else:
+        Exception(f"crowdingcon = {crowdingcon} is not recognized, use 1 or 0")
+
+
 if __name__ == '__main__':
     PATH = "../../data/exp1_rerun_data/"
-    DATA = "cleanedTotalData_fullinfo_v3.xlsx"
+    DATA = "cleanedTotalData_fullinfo_v2.xlsx"
     mydata = pd.read_excel(PATH + DATA)
+
+    # mean crowding vs. no-crowding
+    crowdingcon = 0
+    cal_ds_mean(mydata, crowdingcon = crowdingcon)
+    cal_ds_std(mydata, crowdingcon = crowdingcon)
 
     # 2 way annova
     aov = pg.rm_anova(dv = "deviation_score",
@@ -21,8 +50,8 @@ if __name__ == '__main__':
                       data = mydata)
     # post hoc
     posthocs = pg.pairwise_ttests(dv = "deviation_score",
-                                   within = ["winsize", "crowdingcons"],
-                                   subject = "participant_N",
-                                   data = mydata,
-                                   padjust = "holm",
-                                   effsize = "cohen")
+                                  within = ["winsize", "crowdingcons"],
+                                  subject = "participant_N",
+                                  data = mydata,
+                                  padjust = "fdr_bh",
+                                  effsize = "cohen")

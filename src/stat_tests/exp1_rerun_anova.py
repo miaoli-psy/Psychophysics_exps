0bafa53cdc1159be3e2e05c6b8dd92b796804541
miaoli-psy <chuoli223@yahoo.com>
2021-04-20 08:14:44 +0200

    add pivot table option

--- src/stat_tests/exp1_rerun_anova.py
+++ src/stat_tests/exp1_rerun_anova.py
@@ -15,6 +15,12 @@ if __name__ == '__main__':
     PATH = "../../data/exp1_rerun_data/"
     DATA = "cleanedTotalData_fullinfo_v2.xlsx"
     mydata = pd.read_excel(PATH + DATA)
+    pivot_t = False
+
+    if pivot_t:
+        pivot_t = pd.pivot_table(mydata, index = ["crowdingcons", "participant_N"], columns = ["winsize"],
+                                 values = "deviation_score")
+        pivot_t.to_csv("pt_exp1.csv")
 
     # mean crowding vs. no-crowding
     crowdingcon = 0

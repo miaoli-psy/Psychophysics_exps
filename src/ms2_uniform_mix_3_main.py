728853823d7cef3d4fa62282cb0eb493f2a8b38a
miaoli-psy <chuoli223@yahoo.com>
2022-01-09 00:05:10 +0100

    fix: add contrast as a variable

--- src/ms2_uniform_mix_3_main.py
+++ src/ms2_uniform_mix_3_main.py
@@ -15,6 +15,22 @@ def get_percent_triplets(percentpairs):
         raise Exception(f"percentpair {percentpairs} is unexpected")
 
 
+def convert_blockOrdertocontrast1(blockOrder: str):
+    if blockOrder == "c4_mix.xlsx" or blockOrder == "c6_mix.xlsx":
+        return "mix"
+    else:
+        return "uniform"
+
+
+def convert_blockOrdertocontrast2(blockOrder: str):
+    if blockOrder == "c4_mix.xlsx" or blockOrder == "c6_mix.xlsx":
+        return "mix"
+    elif blockOrder == "c4_white.xlsx" or blockOrder == "c6_white_xlsx":
+        return "white"
+    else:
+        return "black"
+
+
 if __name__ == '__main__':
     to_excel = False
 
@@ -25,6 +41,9 @@ if __name__ == '__main__':
 
     # convert percentpairs to percent_triplets
     insert_new_col(data, "perceptpairs", "percent_triplets", get_percent_triplets)
+    insert_new_col(data, "blockOrder", "contrast", convert_blockOrdertocontrast1)
+    insert_new_col(data, "blockOrder", "contrast_full", convert_blockOrdertocontrast2)
+
 
     dv = "deviation_score"
     dv2 = "percent_change"
@@ -33,12 +52,14 @@ if __name__ == '__main__':
     indv2 = "protectzonetype"
     indv3 = "winsize"
     indv4 = "percent_triplets"
-    indv5 = "participant"
+    indv5 = "contrast"
+    indv6 = "contrast_full"
+    indv7 = "participant"
 
     # average data: average deviation and percent change for each condition per participant
-    data_1 = data.groupby([indv, indv2, indv3, indv4, indv5])[[dv, dv2]] \
+    data_1 = data.groupby([indv, indv2, indv3, indv4, indv5, indv6, indv7])[[dv, dv2]] \
         .agg({dv: ['mean', 'std'], dv2: ['mean', 'std']}) \
-        .reset_index(level = [indv, indv2, indv3, indv4, indv5])
+        .reset_index(level = [indv, indv2, indv3, indv4, indv5, indv6, indv7])
 
     # transfer column name
     data_1.columns = [''.join(x) for x in data_1.columns]
@@ -48,21 +69,21 @@ if __name__ == '__main__':
     insert_new_col_from_two_cols(data_1, "percent_changestd", "samplesize", "SEM_percent_change", cal_SEM)
 
     # averaged across participant
-    data_2 = data.groupby([indv, indv2, indv3, indv4])[[dv, dv2]] \
+    data_2 = data.groupby([indv, indv2, indv3, indv4, indv5, indv6])[[dv, dv2]] \
         .agg({dv: ['mean', 'std'], dv2: ['mean', 'std']}) \
-        .reset_index(level = [indv, indv2, indv3, indv4])
+        .reset_index(level = [indv, indv2, indv3, indv4, indv5, indv6])
 
     # transfer column name
     data_2.columns = [''.join(x) for x in data_2.columns]
 
-    data_2["samplesize"] = [19*4] * data_2.shape[0]  # 4 displays * 19 participants
+    data_2["samplesize"] = [19 * 4] * data_2.shape[0]  # 4 displays * 19 participants
     insert_new_col_from_two_cols(data_2, "deviation_scorestd", "samplesize", "SEM_deviation_score", cal_SEM)
     insert_new_col_from_two_cols(data_2, "percent_changestd", "samplesize", "SEM_percent_change", cal_SEM)
 
     # average data: average deviation and percent change for for different winsize per participant
-    data_3 = data.groupby([indv2, indv3, indv4, indv5])[[dv, dv2]] \
+    data_3 = data.groupby([indv2, indv3, indv4, indv5, indv6, indv7])[[dv, dv2]] \
         .agg({dv: ['mean', 'std'], dv2: ['mean', 'std']}) \
-        .reset_index(level = [indv2, indv3, indv4, indv5])
+        .reset_index(level = [indv2, indv3, indv4, indv5, indv6, indv7])
 
     # transfer column name
     data_3.columns = [''.join(x) for x in data_3.columns]
@@ -72,19 +93,19 @@ if __name__ == '__main__':
     insert_new_col_from_two_cols(data_3, "percent_changestd", "samplesize", "SEM_percent_change", cal_SEM)
 
     # average per winsize across participant
-    data_4 = data.groupby([indv2, indv3, indv4])[[dv, dv2]] \
+    data_4 = data.groupby([indv2, indv3, indv4, indv5, indv6])[[dv, dv2]] \
         .agg({dv: ['mean', 'std'], dv2: ['mean', 'std']}) \
-        .reset_index(level = [indv2, indv3, indv4])
+        .reset_index(level = [indv2, indv3, indv4, indv5, indv6])
 
     # transfer column name
     data_4.columns = [''.join(x) for x in data_4.columns]
 
-    data_4["samplesize"] = [12*19] * data_4.shape[0]  # 4 displays * 3 clustering * 19 participants
+    data_4["samplesize"] = [12 * 19] * data_4.shape[0]  # 4 displays * 3 clustering * 19 participants
     insert_new_col_from_two_cols(data_4, "deviation_scorestd", "samplesize", "SEM_deviation_score", cal_SEM)
     insert_new_col_from_two_cols(data_4, "percent_changestd", "samplesize", "SEM_percent_change", cal_SEM)
 
     if to_excel:
-        data_1.to_excel("ms2_uniform_mix_3_data_each_pp.xlsx", index = False)
+        data_1.to_excel("ms2_uniform_mix_3_each_pp.xlsx", index = False)
         data_2.to_excel("ms2_uniform_mix_3.xlsx", index = False)
         data_3.to_excel("ms2_uniform_mix_3_combine_num_each_pp.xlsx", index = False)
         data_4.to_excel("ms2_uniform_mix_3_combine_num.xlsx", index = False)

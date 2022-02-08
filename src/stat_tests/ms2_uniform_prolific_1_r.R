c5edcc7590f8f5535ee55e9478b8476a95a518ff
miaoli-psy <chuoli223@yahoo.com>
2022-02-08 11:26:20 +0100

    refactor ms2 exp1 r analysis

--- src/stat_tests/ms2_uniform_prolific_1_r.R
+++ src/stat_tests/ms2_uniform_prolific_1_r.R
@@ -17,7 +17,8 @@ library(r2glmm)
 setwd("D:/SCALab/projects/numerosity_exps/src/stat_tests/")
 
 # read data
-all_data_each_pp <- read_excel("../../data/ms2_uniform_prolific_1_data/prolifc_data_each_pp.xlsx")
+all_data_each_pp <-
+  read_excel("../../data/ms2_uniform_prolific_1_data/prolifc_data_each_pp.xlsx")
 
 # separate groups
 all_data_small_num <- subset(all_data_each_pp, winsize == 0.4)
@@ -32,30 +33,30 @@ summary(data)
 # Visualization------------------------------------------------------
 
 # subject
-bxp <- ggboxplot(data = all_data_each_pp, 
-                 x = "participant", 
-                 y = "mean_deviation_score", 
+bxp <- ggboxplot(data = all_data_each_pp,
+                 x = "participant",
+                 y = "mean_deviation_score",
                  color = "protectzonetype") +
-  facet_wrap(~ winsize, nrow = 2)
+  facet_wrap( ~ winsize, nrow = 2)
 
 print(bxp)
 
 # clustering level
-bxp2 <- ggboxplot(data = all_data_each_pp, 
-                  x = "percent_triplets", 
-                  y = "mean_deviation_score", 
+bxp2 <- ggboxplot(data = all_data_each_pp,
+                  x = "percent_triplets",
+                  y = "mean_deviation_score",
                   color = "protectzonetype") +
-  facet_wrap(~ winsize, ncol = 2)
+  facet_wrap( ~ winsize, ncol = 2)
 
 print(bxp2)
 
 # numerosity
 
-bxp3 <- ggboxplot(data = all_data_each_pp, 
-                  x = "numerosity", 
-                  y = "mean_deviation_score", 
+bxp3 <- ggboxplot(data = all_data_each_pp,
+                  x = "numerosity",
+                  y = "mean_deviation_score",
                   color = "protectzonetype") +
-  facet_wrap(~ winsize, nrow = 2, scales = "free")
+  facet_wrap( ~ winsize, nrow = 2, scales = "free")
 
 print(bxp3)
 
@@ -65,33 +66,45 @@ print(bxp3)
 # ID as factor
 str(data)
 
-data$percent_triplets <-as.factor(data$percent_triplets)
-data$protectzonetype <-as.factor(data$protectzonetype)
-data$numerosity <-as.factor(data$numerosity)
+data$percent_triplets <- as.factor(data$percent_triplets)
+data$protectzonetype <- as.factor(data$protectzonetype)
+data$numerosity <- as.factor(data$numerosity)
 
 
-alignment_con.model1 <- lmer(mean_deviation_score ~ protectzonetype + 
-                               percent_triplets + 
-                               (1|participant) + 
-                               (1|numerosity), 
-                             data = data, REML = FALSE)
+alignment_con.model1 <-
+  lmer(
+    mean_deviation_score ~ protectzonetype +
+      percent_triplets +
+      (1 | participant) +
+      (1 | numerosity),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.model1
 
 
-alignment_con.null1 <- lmer(mean_deviation_score ~ percent_triplets + 
-                              (1|participant) + 
-                              (1|numerosity), 
-                            data = data, REML = FALSE)
+alignment_con.null1 <-
+  lmer(
+    mean_deviation_score ~ percent_triplets +
+      (1 | participant) +
+      (1 | numerosity),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.null1
 
 
 anova(alignment_con.model1, alignment_con.null1)
 
 # check interaction: no interaction
-alignment_con.interaction <- lmer(mean_deviation_score ~ protectzonetype * percent_triplets + 
-                                    (1|participant) + 
-                                    (1|numerosity), 
-                                  data = data, REML = FALSE)
+alignment_con.interaction <-
+  lmer(
+    mean_deviation_score ~ protectzonetype * percent_triplets +
+      (1 | participant) +
+      (1 | numerosity),
+    data = data,
+    REML = FALSE
+  )
 
 anova(alignment_con.interaction, alignment_con.model1)
 
@@ -99,60 +112,89 @@ anova(alignment_con.interaction, alignment_con.model1)
 
 coef(alignment_con.model)
 
-alignment_con.model_random_slope <- lmer(mean_deviation_score ~ protectzonetype + percent_triplets + 
-                                           (1 + protectzonetype|participant) + 
-                                           (1 + protectzonetype|numerosity), 
-                                         data = data, REML = FALSE)
+alignment_con.model_random_slope <-
+  lmer(
+    mean_deviation_score ~ protectzonetype + percent_triplets +
+      (1 + protectzonetype |
+         participant) +
+      (1 + protectzonetype |
+         numerosity),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.model_random_slope
 
 
 coef(alignment_con.model)
 
-alignment_con.null_random_slope <- lmer(mean_deviation_score ~ percent_triplets + 
-                                          (1 + protectzonetype|participant) + 
-                                          (1 + protectzonetype|numerosity), 
-                                        data = data, REML = FALSE)
+alignment_con.null_random_slope <-
+  lmer(
+    mean_deviation_score ~ percent_triplets +
+      (1 + protectzonetype |
+         participant) +
+      (1 + protectzonetype |
+         numerosity),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.null_random_slope
 
 
-anova(alignment_con.model_random_slope, alignment_con.null_random_slope)
+anova(alignment_con.model_random_slope,
+      alignment_con.null_random_slope)
 
 
 
 # numerosity not as random effect
 
-alignment_con.model2 <- lmer(mean_deviation_score ~ protectzonetype + 
-                               percent_triplets + 
-                               numerosity + 
-                               (1+ protectzonetype|participant),
-                             data = data, REML = FALSE)
+alignment_con.model2 <-
+  lmer(
+    mean_deviation_score ~ protectzonetype +
+      percent_triplets +
+      numerosity +
+      (1 + protectzonetype | participant),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.model2
 
 
 coef(alignment_con.model)
 
-alignment_con.null2 <- lmer(mean_deviation_score ~ percent_triplets + 
-                              numerosity +
-                              (1+ protectzonetype|participant),
-                            data = data, REML = FALSE)
-alignment_con.null2 
+alignment_con.null2 <-
+  lmer(
+    mean_deviation_score ~ percent_triplets +
+      numerosity +
+      (1 + protectzonetype | participant),
+    data = data,
+    REML = FALSE
+  )
+alignment_con.null2
 
 anova(alignment_con.model2, alignment_con.null2)
 
 
 
-alignment_con.model4 <- lmer(mean_deviation_score ~ protectzonetype + 
-                               numerosity + 
-                               percent_triplets + 
-                               (1 + protectzonetype|participant),
-                             data = data, REML = FALSE)
+alignment_con.model4 <-
+  lmer(
+    mean_deviation_score ~ protectzonetype +
+      numerosity +
+      percent_triplets +
+      (1 + protectzonetype | participant),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.model4
 
 
-alignment_con.null4 <- lmer(mean_deviation_score ~ percent_triplets + 
-                              numerosity +
-                              (1 +protectzonetype|participant),
-                            data = data, REML = FALSE)
+alignment_con.null4 <-
+  lmer(
+    mean_deviation_score ~ percent_triplets +
+      numerosity +
+      (1 + protectzonetype | participant),
+    data = data,
+    REML = FALSE
+  )
 alignment_con.null4
 
 anova(alignment_con.model4, alignment_con.null4)
@@ -169,7 +211,11 @@ r.squaredGLMM(alignment_con.model4)
 r2beta(alignment_con.model4, method = 'kr', partial = TRUE)
 
 # posc-hoc on models not on data set (maybe: https://cran.r-project.org/web/packages/emmeans/vignettes/interactions.html)
-emmeans(alignment_con.model_random_slope, list(pairwise ~ protectzonetype * percent_triplets), adjust = "tukey")
+emmeans(
+  alignment_con.model_random_slope,
+  list(pairwise ~ protectzonetype * percent_triplets),
+  adjust = "tukey"
+)
 
 
 # troditional ANOVA
@@ -178,18 +224,29 @@ emmeans(alignment_con.model_random_slope, list(pairwise ~ protectzonetype * perc
 # read data
 
 # ANOVA
-data_anova <- read_excel("../../data/ms2_uniform_prolific_1_data/prolifc_data_combine_cluster_each_pp.xlsx")
+data_anova <-
+  read_excel(
+    "../../data/ms2_uniform_prolific_1_data/prolifc_data_combine_cluster_each_pp.xlsx"
+  )
 data_anova <- subset(data_anova, winsize == 0.6)
 
-res.anova <- aov(mean_deviation_score ~ numerosity + protectzonetype + numerosity:protectzonetype, data = data_anova)
+res.anova <-
+  aov(
+    mean_deviation_score ~ numerosity + protectzonetype + numerosity:protectzonetype,
+    data = data_anova
+  )
 Anova(res.anova, type = "III")
 
 
-res.mol1 <- lmer(mean_deviation_score ~ percent_triplets + protectzonetype + (1|participant), data = data_anova)
-res.null <- lmer(mean_deviation_score ~ percent_triplets + (1|participant), data = data_anova)
-anova(res.null,res.mol1)
+res.mol1 <-
+  lmer(mean_deviation_score ~ percent_triplets +
+         protectzonetype + (1 | participant),
+       data = data_anova)
+res.null <-
+  lmer(mean_deviation_score ~ percent_triplets +
+         (1 | participant),
+       data = data_anova)
+anova(res.null, res.mol1)
 
 r.squaredGLMM(res.mol1)
 r2beta(res.mol1, method = 'kr', partial = TRUE)
-
-

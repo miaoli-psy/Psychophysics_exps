e8d002ad5d6f18f68f850157b3b2f829c6c51c5c
miaoli-psy <chuoli223@yahoo.com>
2022-02-28 16:14:22 +0100

    ms2 exp1 2 indv

--- src/ms2_uniform_prolific_1_main.py
+++ src/ms2_uniform_prolific_1_main.py
@@ -37,15 +37,33 @@ if __name__ == '__main__':
     # convert percentpairs to percent_triplets
     insert_new_col(data, "perceptpairs", "percent_triplets", get_percent_triplets)
 
-    # averaged data: averaged deviation for each condition per participant
-    data_1 = data.groupby(["percent_triplets", "numerosity", "protectzonetype", "participant", "winsize"])[
-        "deviation_score"] \
-        .agg(['mean', 'std']) \
-        .reset_index(level = ["percent_triplets", "numerosity", "protectzonetype", "participant", "winsize"])
+    dv = "deviation_score"
+    dv2 = "percent_change"
 
-    rename_df_col(df = data_1, old_col_name = "mean", new_col_name = "mean_deviation_score")
+    indv = "numerosity"
+    indv2 = "protectzonetype"
+    indv3 = "winsize"
+    indv4 = "percent_triplets"
+    indv5 = "perceptpairs"
+    indv6 = "participant"
+
+    # averaged data: averaged deviation for each condition per participant
+    # data_1 = data.groupby(["percent_triplets", "numerosity", "protectzonetype", "participant", "winsize"])[
+    #     "deviation_score"] \
+    #     .agg(['mean', 'std']) \
+    #     .reset_index(level = ["percent_triplets", "numerosity", "protectzonetype", "participant", "winsize"])
+    # rename_df_col(df = data_1, old_col_name = "mean", new_col_name = "mean_deviation_score")
+
+    data_1 = data.groupby([indv, indv2, indv3, indv4, indv5, indv6])[[dv, dv2]] \
+        .agg({dv: ['mean', 'std'], dv2: ['mean', 'std']}) \
+        .reset_index(level = [indv, indv2, indv3, indv4, indv5, indv6])
+
+    # transfer column name
+    data_1.columns = [''.join(x) for x in data_1.columns]
+
     data_1["samplesize"] = [5] * data_1.shape[0]  # each participant repeat each condition 5 times (5 displays)
-    insert_new_col_from_two_cols(data_1, "mean_deviation_score", "samplesize", "SEM", cal_SEM)
+    insert_new_col_from_two_cols(data_1, "deviation_scorestd", "samplesize", "SEM_deviation_score", cal_SEM)
+    insert_new_col_from_two_cols(data_1, "percent_changestd", "samplesize", "SEM_percent_change", cal_SEM)
 
     # averaged data: averaged across participant
     data_2 = data.groupby(["percent_triplets", "numerosity", "protectzonetype", "winsize"])["deviation_score"] \
@@ -91,4 +109,4 @@ if __name__ == '__main__':
     insert_new_col_from_two_cols(data_6, "mean_deviation_score", "samplesize", "SEM", cal_SEM)
 
     if write_to_excel:
-        data_6.to_excel("prolifc_data.xlsx", index = False)
+        data_1.to_excel("prolifc_data.xlsx", index = False)

5f7e234287a5663c3f967af071f0c684811f921f
miaoli-psy <chuoli223@yahoo.com>
2022-07-23 13:32:13 +0200

    add density calculation fuction, be ready for local density cal

---
+++ src/display_properties/properties.py
@@ -0,0 +1,23 @@
+from scipy.spatial import distance, ConvexHull
+from itertools import combinations
+import numpy as np
+
+class Properties:
+    def __init__(self, posilist):
+        self.numerosity = len(posilist)
+        self.__posilist_array = np.asarray(posilist)
+        self.__hull = ConvexHull(self.__posilist_array)
+        self.occupancy_area = round(self.__hull.volume * ((0.25 / 3.82) ** 2), 2)
+        self.density = round(len(posilist)/self.occupancy_area, 4)
+
+
+if __name__ == "__main__":
+    debug = True
+    if debug:
+        properties = Properties([(20, 0), (25, 5), (100, 75), (50, 50), (-75, 65)])
+        a = properties.numerosity
+        b = properties.occupancy_area
+        e = properties.density
+        print("numerosity is", a, ";",
+              "occupancy area is", b, ";",
+              "density is", e, "item/cm2")
